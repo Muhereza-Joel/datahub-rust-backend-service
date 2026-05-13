9d1e7c660af8fb9aefdb95dfe39d7dfcb127cdb2
Muhereza-Joel <muherezajoel40@gmail.com>
2026-05-13 18:18:52 +0300

    update port

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, App, HttpServer, Responder};
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    "Backend running!"
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let port = std::env::var("PORT")
+        .unwrap_or_else(|_| "8080".to_string());
+
+    println!("Starting server on port {}", port);
+
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+    })
+    .bind(format!("0.0.0.0:{}", port))?
+    .run()
+    .await
 }
